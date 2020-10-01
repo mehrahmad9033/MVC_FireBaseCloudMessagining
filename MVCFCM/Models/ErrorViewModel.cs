05544a8646e1a3fe6e6b76cb3bb7784f7336a8d4
Ahmad Mukhtar <mehrahmad9033@gmail.com>
2020-10-01 17:42:15 +0400

    Add project files.

---
+++ MVCFCM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCFCM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
