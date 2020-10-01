dd7d2827657dce614e3bae634ff1fe375f292761
Ahmad Mukhtar <mehrahmad9033@gmail.com>
2020-10-01 23:36:19 +0400

    Added notification Details

--- MVCFCM/Controllers/HomeController.cs
+++ MVCFCM/Controllers/HomeController.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using FirebaseAdmin;
+using FirebaseAdmin.Messaging;
+using Google.Apis.Auth.OAuth2;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MVCFCM.Models;
+using Newtonsoft.Json;
 
 namespace MVCFCM.Controllers
 {
@@ -18,11 +24,90 @@ namespace MVCFCM.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var defaultApp = FirebaseApp.Create(new AppOptions()
+            {
+                Credential = GoogleCredential.FromFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "key.json")),
+            });
+            var message = new Message()
+            {
+                Data = new Dictionary<string, string>()
+                {
+                    ["FirstName"] = "John",
+                    ["LastName"] = "Doe"
+                },
+                Notification = new Notification
+                {
+                    Title = "Message Title",
+                    Body = "Message Body"
+                },
+
+                //Token = "d3aLewjvTNw:APA91bE94LuGCqCSInwVaPuL1RoqWokeSLtwauyK-r0EmkPNeZmGavSG6ZgYQ4GRjp0NgOI1p-OAKORiNPHZe2IQWz5v1c3mwRE5s5WTv6_Pbhh58rY0yGEMQdDNEtPPZ_kJmqN5CaIc",
+                Topic = "news"
+            };
+            var messaging = FirebaseMessaging.DefaultInstance;
+            var result = await messaging.SendAsync(message);
             return View();
         }
 
+        public void SendNotification(string DeviceID,string Title, string Body, string Link, string SERVER_API_KEY,string SENDER_ID)
+        {
+            try
+            {
+                dynamic data = new
+                {
+                    to = "YOUR_FCM_DEVICE_ID", // Uncoment this if you want to test for single device
+                                             // registration_ids = singlebatch, // this is for multiple user 
+                    notification = new
+                    {
+                        title = "FCM Test",     // Notification title
+                        body = "FCM Test Succeeded",    // Notification body data
+                        link = "https://"       // When click on notification user redirect to this link
+                    }
+                };
+
+                //var serializer = new JavaScriptSerializer();// System.Web.Script.Serialization.JavaScriptSerializer();
+                var json = JsonConvert.SerializeObject(data);
+                Byte[] byteArray = System.Text.Encoding.UTF8.GetBytes(json);
+
+                
+                WebRequest tRequest;
+                tRequest = WebRequest.Create("https://fcm.googleapis.com/fcm/send");
+                tRequest.Method = "post";
+                tRequest.ContentType = "application/json";
+                tRequest.Headers.Add(string.Format("Authorization: key={0}", SERVER_API_KEY));
+
+                tRequest.Headers.Add(string.Format("Sender: id={0}", SENDER_ID));
+
+                tRequest.ContentLength = byteArray.Length;
+                Stream dataStream = tRequest.GetRequestStream();
+                dataStream.Write(byteArray, 0, byteArray.Length);
+                dataStream.Close();
+
+                WebResponse tResponse = tRequest.GetResponse();
+
+                dataStream = tResponse.GetResponseStream();
+
+                StreamReader tReader = new StreamReader(dataStream);
+
+                String sResponseFromServer = tReader.ReadToEnd();
+
+                tReader.Close();
+                dataStream.Close();
+                tResponse.Close();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        [HttpPost]
+        public IActionResult Send()
+        {
+            ViewBag.Message = "Notification sent successfully";
+            return View("Index");
+        }
         public IActionResult Privacy()
         {
             return View();
